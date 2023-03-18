b0276e8e81552f40eddc5fee281db657f9cd1b0c
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2023-03-18 11:42:38 -0300

    feat: :sparkles: adicionando o componente list

---
+++ src/components/List/index.tsx
@@ -0,0 +1,33 @@
+function List() {
+  const tarefas = [
+    {
+      id: 1,
+      title: "React",
+      time: "02:00:00",
+    },
+    {
+      id: 2,
+      title: "Javascript",
+      time: "01:00:00",
+    },
+    {
+      id: 3,
+      title: "Typescript",
+      time: "01:00:00",
+    },
+  ];
+  return (
+    <aside>
+      <h2>Estudos do dia</h2>
+      <ul>
+        {tarefas.map((tarefa) => (
+          <li key={tarefa.id}>
+            <h3>{tarefa.title}</h3>
+            <span>{tarefa.time}</span>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
+}
+export default List;
